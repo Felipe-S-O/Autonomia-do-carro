02eca6fbd6c15c0e8d7cb660d3324e51dac1ee9d
Felipe <felipe.silva412897@gmail.com>
2020-05-26 10:06:13 -0300

    criando atributo

--- carroAutonomia/src/main/java/autonomiaCarro/Carro.java
+++ carroAutonomia/src/main/java/autonomiaCarro/Carro.java
@@ -6,5 +6,11 @@ package autonomiaCarro;
  * @author felipe
  */
 public class Carro {
-    
+  private  String marca; 
+  private  String modelo;
+  private  int numPassageiro;  //número de passageiros
+  private  double capacidadeComb;  //capacidade do tanque de  combustível 
+  private  double consumoComb; //consumo de combustível por km
+  
+
 }
