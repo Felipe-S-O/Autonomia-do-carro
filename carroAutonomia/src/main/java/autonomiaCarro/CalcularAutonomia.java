9ed4147add5bcbb75268056878811d2dd0e3ef2e
Felipe <felipe.silva412897@gmail.com>
2020-05-26 10:46:30 -0300

    autonomia criada

--- carroAutonomia/src/main/java/autonomiaCarro/CalcularAutonomia.java
+++ carroAutonomia/src/main/java/autonomiaCarro/CalcularAutonomia.java
@@ -7,11 +7,35 @@ package autonomiaCarro;
  */
 public class CalcularAutonomia {
     
-    public static void main(String[] args){
+ public static void main(String[] args){
         
+        Carro van = new Carro(); 
+       
+        van.marca = "fiat";
+        van.modelo = "Ducato";
+        van.numPassageiros = 10;
+        van.copCombustivel = 50;
+        van.consumoCombustivel = 1.6;
         
+        System.out.println(van.marca);
+        System.out.println(van.modelo);
+        System.out.println(van.numPassageiros);
+        System.out.println(van.copCombustivel);
+        System.out.println(van.consumoCombustivel);
+        
+        van.exibirAutonomia();
+        
+        System.out.println();
+        
+        double autonomia = van.obterAutonomia();
+        System.out.println("A autonomia do carro é: " + autonomia);
+        System.out.println();
+        System.out.println(van.obterAutonomia());
+        
+       double qtd10km = van.calcularCombustivel(10);
+        
+        System.out.println("qtdCombustivel10 = " + qtd10km);
     }
     
- 
     
 }
--- carroAutonomia/src/main/java/autonomiaCarro/Carro.java
+++ carroAutonomia/src/main/java/autonomiaCarro/Carro.java
@@ -6,11 +6,26 @@ package autonomiaCarro;
  * @author felipe
  */
 public class Carro {
-  private  String marca; 
-  private  String modelo;
-  private  int numPassageiro;  //número de passageiros
-  private  double capacidadeComb;  //capacidade do tanque de  combustível 
-  private  double consumoComb; //consumo de combustível por km
-  
-
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double copCombustivel;
+    double consumoCombustivel; 
+    
+    void exibirAutonomia(){
+        System.out.println("A autonomia do carro é: " + copCombustivel * consumoCombustivel + "km");
+           
+    }
+    double obterAutonomia(){
+        System.out.println("méto obterAutonomia foi chamando");
+        
+        return copCombustivel * consumoCombustivel;
+    }
+    
+    double calcularCombustivel(double km){
+        
+        double qtdCombustivel = km/consumoCombustivel;
+        
+        return qtdCombustivel;
+    }
 }
