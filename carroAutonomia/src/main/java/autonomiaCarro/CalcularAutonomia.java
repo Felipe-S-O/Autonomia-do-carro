f6b1cc861847939539334b6c40df586f17b4c444
Felipe <felipe.silva412897@gmail.com>
2020-05-26 02:25:48 -0300

    criando public static

---
+++ carroAutonomia/src/main/java/autonomiaCarro/CalcularAutonomia.java
@@ -0,0 +1,17 @@
+
+package autonomiaCarro;
+
+/**
+ *
+ * @author felipe
+ */
+public class CalcularAutonomia {
+    
+    public static void main(String[] args){
+        
+        
+    }
+    
+ 
+    
+}
